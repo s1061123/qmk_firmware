8cd0dc505ac00c444f97ab7a23162408f566ab18
Tomofumi Hayashi <tohayash@redhat.com>
2020-02-21 21:24:52 +0900

    fix

--- keyboards/ergodash/rev1/keymaps/s1061123/keymap.c
+++ keyboards/ergodash/rev1/keymaps/s1061123/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME,                        KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_END,                         KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    LOWER,   RAISE,   KC_LGUI, KC_LALT,          KC_SPC,  KC_BSPC,KC_DEL,         KC_BSPC,KC_ENT , KC_SPC,           KC_LBRC, KC_RCBC, KC_MINS,   KC_EQL \
+    LOWER,   RAISE,   KC_LGUI, KC_LALT,          KC_SPC,  KC_BSPC,KC_DEL,         KC_BSPC,KC_ENT , KC_SPC,           KC_LBRC, KC_RBRC, KC_MINS,   KC_EQL \
   ),
 
   /* Lower
