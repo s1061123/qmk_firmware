b22da9c983c9318615e815c6dd7e86c3a2153b48
Tomofumi Hayashi <tohayash@redhat.com>
2024-05-24 14:51:30 +0900

    mod

--- keyboards/ergodox_ez/keymaps/s1061123/keymap.c
+++ keyboards/ergodox_ez/keymaps/s1061123/keymap.c
@@ -22,10 +22,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | GRV `  |   1  |   2  |   3  |   4  |   5  |  6   |           |   5  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           | PgUp |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | HOME |           | PgUp |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCTRL  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------| Hyper|           | PgDn |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| END  |           | PgDn |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |/L2   |  /L1 | WIN  | LALT | Space|                                       |Space |   [  |   ]  |   [  |  =   |
@@ -41,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
   KC_GRV,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_6,                 KC_5,         KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             KC_PGUP,      KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
+  KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_HOME,              KC_PGUP,      KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
   KC_LCTL,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,              KC_SCLN,        KC_QUOT,
-  KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),         KC_PGDN,      KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_SLSH,        KC_RSFT,
+  KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_END,               KC_PGDN,      KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_SLSH,        KC_RSFT,
   LT(MDIA,KC_NO),LT(SYMB,KC_NO),KC_LGUI,       KC_LALT, KC_SPC,                                               KC_SPC,  KC_LBRC, KC_RBRC, KC_LBRC, KC_EQL,
-                                                           ALT_T(KC_APP), KC_LGUI,                KC_LALT,          KC_ESC,
-                                                                          KC_HOME,                 KC_PGUP,
-                                                         KC_BSPC, KC_DELT,KC_END,                  KC_PGDN, KC_RALT, KC_ENT
+                                                        ALT_T(KC_APP),    KC_LGUI,              KC_LALT,      KC_ESC,
+                                                                          KC_HOME,              KC_PGUP,
+                                                        KC_BSPC,   KC_DELT,KC_END,              KC_PGDN, KC_RALT, KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
